6ed02ab027b9a1126ce0f01c1ffc769d0ea3075c
Richard Burhans <burhans@bx.psu.edu>
2021-07-08 15:18:38 -0400

    ruby

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -26,8 +26,8 @@ echo "INFO: finished setting up rvm environment"
 
 echo "INFO: installing ruby-${RUBY_RELEASE}"
 echo "## $HOME/.rvmrc"
-cat "$HOME/.rvmrc"
 rvm install "${RUBY_RELEASE}"
+rvm alias default "ruby-${ RUBY_RELEASE}"
 echo "# rvm alias list"
 rvm alias list
 echo "# rvm list"
